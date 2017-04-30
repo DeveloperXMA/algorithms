d2d884cb94f719f11e9e8ef77ef90dc9b84c7460
xinruima <fullstackguy@gmail.com>
2017-04-29 23:55:51 -0400

    's'

--- dot_product/DotProduct.java
+++ dot_product/DotProduct.java
@@ -1,10 +1,51 @@
 import java.util.*;
 
 public class DotProduct {
+
+    private static void merge(int[] data, int start, int mid, int end) {
+      int[] temp = new int[data.length];
+      int start1 = start, end1 = mid;
+      int start2 = mid + 1, end2 = end;
+      int index = start;
+      while (start1 <= end1 && start2 <= end2 ) {
+        temp[index++] = (data[start1] > data[start2]) ? data[start1++] : data[start2++];
+      }
+      while (start1 <= end1) {
+        temp[index++] = data[start1++];
+      }
+      while (start2 <= end2) {
+        temp[index++] = data[start2++];
+      }
+      for (int i = start; i <= end; i++) {
+        data[i] = temp[i];
+      }
+    }
+
+    private static void sort(int[] data, int start, int end) {
+      if (start >= end) return;
+      int length = end - start;
+      int mid = (length >> 1) + start;
+      int start1 = start;
+      int end1 = mid;
+      int start2 = mid + 1;
+      int end2 = end;
+      sort(data, start1, end1);
+      sort(data, start2, end2);
+      merge(data, start, mid, end);
+    }
+
+    private static void mergeSort(int[] data) {
+      sort(data, 0, data.length - 1);
+    }
+
     private static long maxDotProduct(int[] a, int[] b) {
         //write your code here
+        mergeSort(a);
+        mergeSort(b);
         long result = 0;
         for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+            System.out.println(b[i]);
             result += a[i] * b[i];
         }
         return result;
@@ -24,4 +65,3 @@ public class DotProduct {
         System.out.println(maxDotProduct(a, b));
     }
 }
-
